b484736e87f698df1e56c78af6d109c89c001ce9
Juan David Aristizábal <jd.ac@outlook.com>
2017-07-21 18:26:23 -0500

    create progress model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
          :token_authenticatable
 
   has_many :authentication_tokens
+  has_many :progresses
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170721225919) do
+ActiveRecord::Schema.define(version: 20170721232422) do
 
   create_table "authentication_tokens", force: :cascade do |t|
     t.string   "body"
@@ -40,6 +40,15 @@ ActiveRecord::Schema.define(version: 20170721225919) do
     t.string   "thumbnail"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "progresses", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "chapter_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chapter_id"], name: "index_progresses_on_chapter_id"
+    t.index ["user_id"], name: "index_progresses_on_user_id"
   end
 
   create_table "schools", force: :cascade do |t|
