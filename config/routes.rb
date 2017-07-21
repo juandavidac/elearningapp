8d659d522f0562358830cb16c3cf0941f3e85ee7
Juan David Aristizábal <jd.ac@outlook.com>
2017-07-20 23:25:23 -0500

    create courses controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {:sessions => "sessions", :registrations => "registrations"}
 
-  resources :schools do
+  resources :schools
 
-  end
+  resources :courses
+
   get '/my_current_user' => 'users#my_current_user'
   match 'users/:id' => 'users#update_user', via: [:patch]
   get '/send_password' => 'users#reset_password'
